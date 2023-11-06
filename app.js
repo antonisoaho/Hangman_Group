65765946063dbffb9a6dcdbda39ded66b09e71fa
antonisoaho <isoahoanton@gmail.com>
2023-11-06 14:32:59 +0100

    Pushing for us to work on branches

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import { celebrities, cities, animals } from './words.js';
+import { celebrities, cities, animals, mix } from './words.js';
 
-const svgFigure = document.querySelectorAll('svg .hidden');
+const svgField = document.querySelector('svg'),
+  svgFigure = Array.from(svgField.children);
 
 const btnPlay = document.querySelector('btn-play'),
   wordBox = document.querySelector('#secretword'),
@@ -12,18 +13,46 @@ let tryCounter = 0,
   secretWord,
   guessedLetters = [],
   wrongLetters = [],
-  points = 0;
+  points = 0,
+  chosenCategory = mix;
 
-const showPart = (part) => {
-  svgFigure[part].classList.toggle('hidden');
-  tryCounter++;
-  return tryCounter;
+// EventListener för att titta vad användare har gissat på för bokstav
+guessLetter.addEventListener('keyup', (keyPress) => {
+  if (isNaN(guessLetter.value)) {
+    if (!guessedLetters.includes(guessLetter.value.toUpperCase())) {
+      guessLetter.style.color = 'green';
+      if (keyPress.key === 'Enter') {
+        newLetter();
+      }
+    } else {
+      guessLetter.style.color = 'red';
+    }
+  } else {
+    guessLetter.value = '';
+  }
+});
+
+//Funktion som anropas när rätt format på inputfältet är.
+const newLetter = () => {
+  testLetter(guessLetter.value.toUpperCase(), newWord, secretWord);
+  guessLetter.value = '';
 };
 
+//Funktion som visar del av figur
+const showPart = (part) => {
+  svgFigure[part].classList.remove('hidden');
+};
+
+//Funktion som återställer spelet till grundinställningar
 const gameReset = () => {
   tryCounter = 0;
+
+  svgFigure.forEach((part) => {
+    part.classList.add('hidden');
+  });
 };
 
+//Funktion som generar ett nytt ord ur array från vald kategori
 const randomWord = (words) => {
   newWord = words[Math.floor(Math.random() * words.length)].toUpperCase();
 
@@ -36,21 +65,39 @@ const randomWord = (words) => {
 
   secretWord = secretArray.join(' ');
   wordBox.textContent = secretWord;
-  return [secretWord, newWord];
+  return [newWord, secretWord];
 };
 
+//Funktion som visar en box som berättar status och frågar om man vill spela igen
+const endGame = (gameStatus) => {
+  const statusBox = document.querySelector('.status');
+  statusBox.classList.remove('hidden');
+  if (gameStatus) {
+    statusBox.querySelector(
+      'h1'
+    ).textContent = `You won mothafucka, gained: ${points} points`;
+  } else {
+    statusBox.querySelector(
+      'h1'
+    ).textContent = `You lose sucka, gained: ${points} points`;
+  }
+};
+
+//Funktion som testar bokstav samt matas med vårt secretWord och newWord
 const testLetter = (letter, word, secret) => {
   const positions = [];
+  //En loop som tittar om det är några positioner som passar till gissade bokstaven
   for (let i = 0; i < word.length; i++) {
     if (word[i] === letter) {
       positions.push(i);
     }
   }
 
+  //Tittar om det fanns några träffar och byter isf ut _ -> rätt bokstav
   if (positions.length) {
     const result = secret.split('');
     positions.forEach((position) => {
-      if (position < resultArray.length) {
+      if (position < result.length) {
         result[position] = letter;
         points++;
       }
@@ -60,15 +107,28 @@ const testLetter = (letter, word, secret) => {
     wordBox.textContent = resultString;
     secretWord = resultString;
     guessedLetters.push(letter);
+
+    if (!secretWord.includes('_')) {
+      endGame(true);
+    }
+
     return [secretWord, guessedLetters];
   } else {
+    //Om man gissade fel bokstav så får man konsekvenserna här.
+    showPart(tryCounter);
+    tryCounter++;
+
     guessedLetters.push(letter);
     wrongLetters.push(letter);
     letterBox.textContent = wrongLetters;
-    return [guessedLetters, wrongLetters];
+
+    if (tryCounter === 6) {
+      endGame(false);
+    }
+    return [guessedLetters, wrongLetters, tryCounter];
   }
 };
 
-newWord = randomWord(celebrities);
+gameReset();
+[newWord, secretWord] = randomWord(chosenCategory);
 console.log(newWord);
-testLetter('T', newWord, secretWord);
