d5158ffe77683b3c5720296989f3755d6a376a73
reda fezazi <redafezazi1@gmail.com>
2023-11-06 17:27:19 +0100

    Point system

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ let tryCounter = 0,
   guessedLetters = [],
   wrongLetters = [],
   points = 0,
-  chosenCategory = mix;
+  chosenCategory = mix,
+  start = Date.now();
 
 // EventListener för att titta vad användare har gissat på för bokstav
 guessLetter.addEventListener("keyup", (keyPress) => {
@@ -74,9 +75,12 @@ const endGame = (gameStatus) => {
   const statusBox = document.querySelector(".status");
   statusBox.classList.remove("hidden");
   if (gameStatus) {
+    let score = Math.floor(
+      100 - (Date.now() - start) / 10000 - wrongLetters.length * 5
+    );
     statusBox.querySelector(
       "h1"
-    ).textContent = `You won mothafucka, gained: ${points} points`;
+    ).textContent = `You won mothafucka, gained: ${score} points`;
   } else {
     statusBox.querySelector(
       "h1"
@@ -135,9 +139,8 @@ gameReset();
 console.log(newWord);
 
 const startTimer = () => {
-  const start = Date.now();
-
   setInterval(() => {
+    timer.innerHTML = ``;
     const seconds = Math.floor((Date.now() - start) / 1000);
     let minutes = Math.floor(seconds / 60);
     if (minutes < 10) {
